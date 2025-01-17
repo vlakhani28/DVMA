c83d616e883f0eddfd972d1ef96bd1665ad9dc3c
Vaibhav Lakhani <vlakhani28@gmail.com>
2025-01-18 01:26:23 +0530

    Add files via upload

---
+++ Damnn Vulnerable MacOS ApplicationTests/Damnn_Vulnerable_MacOS_ApplicationTests.swift
@@ -0,0 +1,17 @@
+//
+//  Damnn_Vulnerable_MacOS_ApplicationTests.swift
+//  Damnn Vulnerable MacOS ApplicationTests
+//
+//  Created by Vaibhav Lakhani on 26/12/24.
+//
+
+import Testing
+@testable import Damnn_Vulnerable_MacOS_Application
+
+struct Damnn_Vulnerable_MacOS_ApplicationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
